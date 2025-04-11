298967a8e3dc879e82d28cd5e8357257eda62e9c
hubao <kazmha@farmingdale.edu>
2025-04-11 16:58:24 -0400

    mainmenu screen first iteration + controller skeleton with ids and action

---
+++ src/main/java/com/example/csc311finalcapstoneprojectgroup04/MainMenuScreenController.java
@@ -0,0 +1,33 @@
+package com.example.csc311finalcapstoneprojectgroup04;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainMenuScreenController {
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private Button optionsButton;
+
+    @FXML
+    private Button playButton;
+
+    @FXML
+    void exitProgram(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openPlayMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void optionPullUp(ActionEvent event) {
+
+    }
+
+}
