67ca3be038c97192f99554704ef22f752b8841ae
MaximFarmingdale <nikim@farmingdale.edu>
2025-04-23 17:48:10 -0400

    Deleted server's main method as it is no longer necessary

--- src/main/java/com/example/csc311finalcapstoneprojectgroup04/TCPNetworking/Server.java
+++ src/main/java/com/example/csc311finalcapstoneprojectgroup04/TCPNetworking/Server.java
@@ -132,11 +132,7 @@ public class Server  {
             e.printStackTrace();
         }
     }
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(1234);
-        Server server = new Server(serverSocket, "user");
-        server.startServer();
-    }
+
 
 
 }
