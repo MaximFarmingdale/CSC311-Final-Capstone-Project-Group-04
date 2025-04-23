021fa86fc86d9a757c9fe2109fc252e515de7cc0
MaximFarmingdale <nikim@farmingdale.edu>
2025-04-23 18:48:17 -0400

    Added JavaDoc comments to the Server class and did some minor improvements.

--- src/main/java/com/example/csc311finalcapstoneprojectgroup04/TCPNetworking/Server.java
+++ src/main/java/com/example/csc311finalcapstoneprojectgroup04/TCPNetworking/Server.java
@@ -8,14 +8,21 @@ import com.example.csc311finalcapstoneprojectgroup04.User;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 import static com.example.csc311finalcapstoneprojectgroup04.TCPNetworking.ClientHandler.clients;
 
-/// Server class which takes in input from the user and uses a client handler to send those inputs to every user
-/// Make sure to add a way to send over the amount of users later when a server pings the lobby
+/**
+ * This is the Server class that manages a Server socket to allow it to accept multiple connections,
+ * respond to pings, get messages that are sent to it and to send messages back. The class is used
+ * by the host of a lobby and makes use of a lobby object to send a ping response back to a pinging
+ * user.
+ * <p>
+ * It can also send an instance of Ping, Message and Lobby to the sockets that are connected
+ * to it. It uses ClientHandler to manage the lobbies, which by having each client as a different
+ * thread, allows the manging of multiple clients.
+ */
 public class Server  {
     private ServerSocket serverSocket;
     private String username;
@@ -23,8 +30,18 @@ public class Server  {
     private ObjectOutputStream objectOutputStream;
     private ObjectInputStream objectInputStream;
     private Lobby lobby;
+    private ObjectOutputStream pingStream;
 
 
+    /**
+     * Allows you to construct a Server object using the following parameters.
+     * @param serverSocket uses the serversocket to make a new socket that connects to it and creates an objectInputStream and objectOutputStream from it
+     * @param username username from the user object of the host
+     * @param lobby lobby object that the host is also using
+     * @throws IOException Incase there is an error with the objectInputStream and objectOutputStream
+     * the objectInputStream and objectOutputStream are to read the input from users and to output objects to users
+     * it is used throughout the class for these functions
+     */
     public Server(ServerSocket serverSocket, String username, Lobby lobby) throws IOException {
         this.serverSocket = serverSocket;
         this.username = username;
@@ -33,6 +50,12 @@ public class Server  {
         objectOutputStream = new ObjectOutputStream(socketServer.getOutputStream());
         this.lobby = lobby;
     }
+
+    /**
+     * This method accepts and rejects socket connections and also responds to pings.
+     * Accepted connections become new ClientHandler objects and get added to the ClientHandler lst: clients.
+     * Always put this method in an unused thread as it is in an endless loop
+     */
     public void startServer() {
         try {
             while (!serverSocket.isClosed()) {
@@ -50,6 +73,7 @@ public class Server  {
                         //if the application is just pinging the server to see if it works
                         if(object instanceof Ping) {
                             pingResponse(socket);
+                            socket.close();
                         }
 
                     } catch (ClassNotFoundException e) {
@@ -58,19 +82,36 @@ public class Server  {
                 }
             }
         } catch (IOException e) {
-            closeServer();
+            stopServer();
         }
     }
 
+    /**
+     * Stops the server and frees up server and client resources if we run into any IOException or any other exception.
+     */
     public void stopServer() {
         try {
-            if (serverSocket != null) {
+            if (serverSocket != null && !serverSocket.isClosed()) {
                 serverSocket.close();
             }
+            for (ClientHandler client : clients) {
+                client.closeClient(); // assume ClientHandler has this method
+            }
+            if(pingStream != null) {
+                pingStream.close();
+            }
+            if(socketServer != null) {
+                closeSocket();
+            }
         } catch (IOException e) {
-            closeServer();
+            throw new RuntimeException(e);
         }
     }
+
+    /**
+     * Allows the host to send a message to all clients
+     * @param message
+     */
     public void sendMessage(Message message) {
         try {
             for (ClientHandler clientHandler : clients) {
@@ -78,10 +119,15 @@ public class Server  {
             }
         }
         catch (IOException e) {
-            closeServer();
+            closeSocket();
         }
 
     }
+
+    /**
+     * Allows the host to send a user to all clients
+     * @param user
+     */
     public void sendMessage(User user) {
         try {
             for (ClientHandler clientHandler : clients) {
@@ -89,11 +135,15 @@ public class Server  {
             }
         }
         catch (IOException e) {
-            closeServer();
+            closeSocket();
         }
 
     }
-    //I am not sure if this method will be used
+
+    /**
+     * Allows the host to send a ping to all clients
+     * @param ping
+     */
     public void sendMessage(Ping ping) {
         try {
             for (ClientHandler clientHandler : clients) {
@@ -101,23 +151,33 @@ public class Server  {
             }
         }
         catch (IOException e) {
-            closeServer();
+            closeSocket();
         }
 
     }
-    public void pingResponse(Socket socket) {
+
+    /**
+     * internal helper method to respond to pings
+     * @param socket
+     * sends a response ping of GameFull if the game is full and returns GameInProgress if the game is in progress
+     */
+    private void pingResponse(Socket socket) {
         try {
-            ObjectOutputStream responseStream = new ObjectOutputStream(socket.getOutputStream());
+            pingStream = new ObjectOutputStream(socket.getOutputStream());
             if (lobby.fullRace())
-                responseStream.writeObject(Ping.GameFull);
+                pingStream.writeObject(Ping.GameFull);
             else if(lobby.getActiveRace())
-                responseStream.writeObject(Ping.GameInProgress);
+                pingStream.writeObject(Ping.GameInProgress);
         }
         catch (IOException e) {
-            closeServer();
+            closeSocket();
         }
     }
-    public void closeServer() {
+
+    /**
+     * Closes the socket that the server class uses for ObjectOutputStream and ObjectInputStream and frees up resources
+     */
+    public void closeSocket() {
         try {
             if (objectOutputStream != null) {
                 objectOutputStream.close();
