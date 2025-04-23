4f04f3a0a2f65244c23951c766074d061a4a2bd3
MaximFarmingdale <nikim@farmingdale.edu>
2025-04-23 17:46:10 -0400

    Changed the ping to a pure enum

--- src/main/java/com/example/csc311finalcapstoneprojectgroup04/NetworkMessagesandUpdate/Ping.java
+++ src/main/java/com/example/csc311finalcapstoneprojectgroup04/NetworkMessagesandUpdate/Ping.java
@@ -1,15 +1,9 @@
 package com.example.csc311finalcapstoneprojectgroup04.NetworkMessagesandUpdate;
 /// ping enum for sending pings
-public class Ping {
-    enum PingStatus {
+    public enum Ping {
         UserPing, //standard user ping
-        UserJoinPing,//if the
         //server responses
         GameInProgress,
         GameFull,
-        GameNotStarted,
         LobbyFull,
-        HostDoesNotMatch,//not sure if this is necessary
-        SuccessfulGameJoin,
     }
-}
