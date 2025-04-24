134a114aa9167e6f645c0a593881e0e75ba8f073
MaximFarmingdale <nikim@farmingdale.edu>
2025-04-23 21:19:27 -0400

    Added JavaDoc comments to the Ping class

--- src/main/java/com/example/csc311finalcapstoneprojectgroup04/NetworkMessagesandUpdate/Ping.java
+++ src/main/java/com/example/csc311finalcapstoneprojectgroup04/NetworkMessagesandUpdate/Ping.java
@@ -1,6 +1,9 @@
 package com.example.csc311finalcapstoneprojectgroup04.NetworkMessagesandUpdate;
+
+import java.io.Serializable;
+
 /// ping enum for sending pings
-    public enum Ping {
+    public enum Ping implements Serializable {
         UserPing, //standard user ping
         //server responses
         GameInProgress,
