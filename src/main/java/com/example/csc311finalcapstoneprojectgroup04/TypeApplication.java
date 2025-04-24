631ac7e30bd4d1294ce139edc7ee2067d83365ec
MaximFarmingdale <nikim@farmingdale.edu>
2025-04-23 21:20:07 -0400

    Added JavaDoc comments to the TypeApplication class

--- src/main/java/com/example/csc311finalcapstoneprojectgroup04/TypeApplication.java
+++ src/main/java/com/example/csc311finalcapstoneprojectgroup04/TypeApplication.java
@@ -12,15 +12,29 @@ import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.IOException;
-/// Application
+
+/**
+ * JavaFX application that is called by the TypeSpringBootApplication, which launches the
+ * JavaFX components.
+ */
 public class TypeApplication extends Application {
     private ConfigurableApplicationContext applicationContext;
-
+    /**
+     * Override of the intializer method of the JavaFX application, sets the ConfigurableApplicationContext.
+     */
     @Override
     public void init() {
         applicationContext = new SpringApplication(TypeSpringBootApplication.class).run();
     }
 
+    /**
+     * Start method of the application that loads SplashScreenController
+     * @param stage the primary stage for this application, onto which
+     * the application scene can be set.
+     * Applications may create other stages, if needed, but they will not be
+     * primary stages.
+     * @throws IOException
+     */
     @Override
     public void start(Stage stage) throws IOException {
         applicationContext.publishEvent(new StageReadyEvent(stage));
@@ -31,23 +45,35 @@ public class TypeApplication extends Application {
         stage.show();
     }
 
+    /**
+     * Stops the application.
+     */
     @Override
     public void stop() {
         applicationContext.close();
         Platform.exit();
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(TypeApplication.class, args);
-    }
-
+    /**
+     */
     static class StageReadyEvent extends ApplicationEvent {
+        /**
+         * StageReadyEvent constuctor
+         * @param stage
+         */
         public StageReadyEvent(Stage stage) {
             super(stage);
         }
 
+        /**
+         * Gets stage
+         * @return Stage
+         */
         public Stage getStage() {
             return ((Stage) getSource());
         }
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(TypeApplication.class, args);
     }
 }
