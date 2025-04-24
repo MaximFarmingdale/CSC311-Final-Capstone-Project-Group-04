4cf1cc3784e1ba8112b3b8f74a02951c8521fb4a
MaximFarmingdale <nikim@farmingdale.edu>
2025-04-24 12:09:13 -0400

    Added JavaDoc comments to StageInitializer

--- src/main/java/com/example/csc311finalcapstoneprojectgroup04/StageInitializer.java
+++ src/main/java/com/example/csc311finalcapstoneprojectgroup04/StageInitializer.java
@@ -4,6 +4,9 @@ import javafx.stage.Stage;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+/**
+ * Initializes the stage after listening for a stage ready event
+ */
 @Component
 public class StageInitializer implements ApplicationListener<TypeApplication.StageReadyEvent> {
     /**
