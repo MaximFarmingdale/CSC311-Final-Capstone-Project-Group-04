53a5f1d3c03d37a638fbaa5cd6b35ec8c0bf49f2
MaximFarmingdale <nikim@farmingdale.edu>
2025-04-27 12:34:26 -0400

    Made A ClientScreenController and FXML with a method to send the current user to it.

---
+++ src/main/java/com/example/csc311finalcapstoneprojectgroup04/JavaFXControllers/ClientScreenController.java
@@ -0,0 +1,11 @@
+package com.example.csc311finalcapstoneprojectgroup04.JavaFXControllers;
+
+import com.example.csc311finalcapstoneprojectgroup04.Lobby.Lobby;
+import com.example.csc311finalcapstoneprojectgroup04.User;
+
+public class ClientScreenController {
+    private User user;
+    public void enterClientScreen(User user, Lobby lobby) {
+
+    }
+}
