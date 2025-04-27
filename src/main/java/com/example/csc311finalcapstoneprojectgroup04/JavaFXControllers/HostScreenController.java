9da489b14ce6816dd9d77b5f153f82a2c3073f3e
MaximFarmingdale <nikim@farmingdale.edu>
2025-04-27 12:31:42 -0400

    Created A HostScreenController for the hosts view of the game and added a method to send the current user to the controller

---
+++ src/main/java/com/example/csc311finalcapstoneprojectgroup04/JavaFXControllers/HostScreenController.java
@@ -0,0 +1,11 @@
+package com.example.csc311finalcapstoneprojectgroup04.JavaFXControllers;
+
+import com.example.csc311finalcapstoneprojectgroup04.User;
+
+public class HostScreenController {
+    private User user;
+    public void enterHostScreen(User currentUser) {
+        user = currentUser;
+    }
+
+}
