2553fecf37750d65d1b3aa72ac62bed7f9f8a90a
MaximFarmingdale <nikim@farmingdale.edu>
2025-04-27 23:08:49 -0400

    Added GUI elements to WaitRoom.FXML and set register-with-eureka: true in the YML for testing proposes.

--- src/main/java/com/example/csc311finalcapstoneprojectgroup04/JavaFXControllers/ClientScreenController.java
+++ src/main/java/com/example/csc311finalcapstoneprojectgroup04/JavaFXControllers/ClientScreenController.java
@@ -2,6 +2,8 @@ package com.example.csc311finalcapstoneprojectgroup04.JavaFXControllers;
 
 import com.example.csc311finalcapstoneprojectgroup04.Lobby.Lobby;
 import com.example.csc311finalcapstoneprojectgroup04.User;
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
 
 public class ClientScreenController {
     private User user;
