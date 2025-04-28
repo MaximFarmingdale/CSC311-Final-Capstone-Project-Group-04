682605bcdc771d8e9adfab1dd333317c03530d04
MaximFarmingdale <nikim@farmingdale.edu>
2025-04-27 21:02:26 -0400

    Made several changes to WaitingRoomController, added an ObservableList that tracks InstanceInfo and added methods to represent the lobbies as GUI elements and added a Map that maps instanceInfo to GUI elements.

--- src/main/java/com/example/csc311finalcapstoneprojectgroup04/JavaFXControllers/WaitingRoomController.java
+++ src/main/java/com/example/csc311finalcapstoneprojectgroup04/JavaFXControllers/WaitingRoomController.java
@@ -1,13 +1,63 @@
 package com.example.csc311finalcapstoneprojectgroup04.JavaFXControllers;
 
+import com.example.csc311finalcapstoneprojectgroup04.Eureka.ClientEureka;
 import com.example.csc311finalcapstoneprojectgroup04.User;
+import com.netflix.appinfo.InstanceInfo;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.ResourceBundle;
 
 /// controller for the lobby select mode
-
-public class WaitingRoomController {
+//@Controller?
+@Component
+public class WaitingRoomController implements Initializable {
     private User user;
+    @FXML
+    private VBox LobbyVbox;
+    @Autowired
+    private ClientEureka clientEureka;
+    private ObservableList<InstanceInfo> instances;
+    private HashMap<InstanceInfo, HBox> instancesMap;
+    private final int maxPlayers = 8;
     public void enterWaitingRoom(User currentUser) {
         user = currentUser;
+        instances = FXCollections.observableArrayList(clientEureka.fillList());
+
+    }
+    private void fillLobbyVbox() {
+        for (InstanceInfo i : instances) {
+            HBox hbox = new HBox();
+            updateHbox(hbox, i);
+            instancesMap.put(i, hbox);
+            LobbyVbox.getChildren().add(hbox);
+        }
+    }
+    private void updateHbox(HBox hbox, InstanceInfo i) {
+        HBox hbox1 = new HBox();
+        Label label = new Label(i.getMetadata().get("host-name"));
+        Label label2 = new Label(i.getMetadata().get("current-players"));
+        hbox1.getChildren().addAll(label, label2);
+        Label label3 = new Label(i.getMetadata().get("active-game"));
+        hbox1.getChildren().addAll(label);
+        hbox.getChildren().addAll(hbox1);
+    }
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
