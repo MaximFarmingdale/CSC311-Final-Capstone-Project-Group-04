f890351a696e517dc97c0c2ca15b5c6d5e647995
MaximFarmingdale <nikim@farmingdale.edu>
2025-04-30 15:25:40 -0400

    Added the FXML elements from HostScreen.FXML to the controller and assigned them to variables and methods

--- src/main/java/com/example/csc311finalcapstoneprojectgroup04/JavaFXControllers/HostScreenController.java
+++ src/main/java/com/example/csc311finalcapstoneprojectgroup04/JavaFXControllers/HostScreenController.java
@@ -1,17 +1,61 @@
 package com.example.csc311finalcapstoneprojectgroup04.JavaFXControllers;
 
+import com.example.csc311finalcapstoneprojectgroup04.Lobby.Lobby;
 import com.example.csc311finalcapstoneprojectgroup04.User;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
+import java.net.Inet4Address;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.ResourceBundle;
+
 @Component
-public class HostScreenController {
+public class HostScreenController implements Initializable {
     private User user;
+    private Lobby lobby;
     @Autowired
     private ApplicationContext applicationContext;
+    @FXML
+    private VBox messageVbox;
+    @FXML
+    private TextField messageField;
+    @FXML
+    Label raceText;
+    @FXML
+    private TextField raceField;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            lobby = new Lobby(Inet4Address.getLocalHost().getHostAddress(), user.getUsername());
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void SendMessage(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+
+        }
+    }
+
+    @FXML
+    void sendRaceUpdate(KeyEvent event) {
+
+    }
+
     public void enterHostScreen(User currentUser) {
         user = currentUser;
     }
+
 
 }
