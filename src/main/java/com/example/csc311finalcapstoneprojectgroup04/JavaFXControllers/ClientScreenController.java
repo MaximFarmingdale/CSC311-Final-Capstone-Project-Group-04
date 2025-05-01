aa5e9805fc4ac32781d0f9d397a54425836af563
MaximFarmingdale <nikim@farmingdale.edu>
2025-04-30 23:47:43 -0400

    Converted ClientScreen.fxml to be more like the current HostScreen.fxml and added the sample controller skeleton to ClientScreenController.

--- src/main/java/com/example/csc311finalcapstoneprojectgroup04/JavaFXControllers/ClientScreenController.java
+++ src/main/java/com/example/csc311finalcapstoneprojectgroup04/JavaFXControllers/ClientScreenController.java
@@ -3,17 +3,53 @@ package com.example.csc311finalcapstoneprojectgroup04.JavaFXControllers;
 import com.example.csc311finalcapstoneprojectgroup04.Lobby.Lobby;
 import com.example.csc311finalcapstoneprojectgroup04.User;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 @Component
-public class ClientScreenController {
+public class ClientScreenController implements Initializable {
     private User user;
     @Autowired
     private ApplicationContext applicationContext;
+
+    @FXML
+    private TextField messageField, raceField;
+
+    @FXML
+    private VBox messageVbox;
+
+    @FXML
+    private ScrollPane messagefield;
+
+    @FXML
+    private Label typedLabel, untypedLabel;
+
+    @FXML
+    void SendMessage(KeyEvent event) {
+
+    }
+
+    @FXML
+    void sendRaceUpdate(KeyEvent event) {
+
+    }
     public void enterClientScreen(User user, Lobby lobby) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
     }
 }
