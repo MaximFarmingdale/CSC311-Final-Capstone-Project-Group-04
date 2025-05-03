01c2f03afb18f2f1af2dc52d371d7a0581706ad2
MaximFarmingdale <nikim@farmingdale.edu>
2025-05-03 18:44:15 -0400

    Added objectOutputStream and objectInputStream as parameters for Client. Also updated Client To no longer process User and ajusted the send RaceUpdate.
    Had some problems with Git so the next few commits will be wordy.

--- src/main/java/com/example/csc311finalcapstoneprojectgroup04/TCPNetworking/Client.java
+++ src/main/java/com/example/csc311finalcapstoneprojectgroup04/TCPNetworking/Client.java
@@ -44,15 +44,16 @@ public class Client implements Runnable{
      * @param socket the provided socket.
      * @param username the username from username.
      */
-    public Client(Socket socket, String username, VBox messageBox, ObservableList<RaceUpdate> raceUpdates) {
+    public Client(Socket socket, ObjectOutputStream objectOutputStream, ObjectInputStream objectInputStream , String username, VBox messageBox, ObservableList<RaceUpdate> raceUpdates) {
         try {
             this.socket = socket;
-            this.objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-            this.objectInputStream = new ObjectInputStream(socket.getInputStream());
-            this.username = username;
-            objectOutputStream.writeObject(username);//check if it's necessary to send a new line
+            this.objectOutputStream = objectOutputStream;
+            this.objectInputStream = objectInputStream;
+            objectOutputStream.writeObject(new Message(username, "HI"));//check if it's necessary to send a new line
             objectOutputStream.flush();
+            this.username = username;
             this.messageBox = messageBox;
+            this.raceUpdates = raceUpdates;
         } catch (IOException e) {
             closeClient();
         }
@@ -73,15 +74,12 @@ public class Client implements Runnable{
         }
     }
 
-    /**
-     * @deprecated
-     * Allows the client to send users to all other clients and the host.
-     * @param user user message.
-     */
-    public void sendMessage(User user) {
+    public void sendMessage(String message) {
         try {
-            objectOutputStream.writeObject(user);
-            objectOutputStream.flush();
+            if (socket.isConnected()) {
+                objectOutputStream.writeObject(message);
+                objectOutputStream.flush();
+            }
         } catch (IOException e) {
             closeClient();
         }
@@ -92,12 +90,13 @@ public class Client implements Runnable{
      * @param raceUpdate
      */
     public void sendMessage(RaceUpdate raceUpdate) {
-        try {
-            objectOutputStream.writeObject(raceUpdate);
-            objectOutputStream.flush();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        for (int i = 0; i < raceUpdates.size(); i++) {
+            if (raceUpdates.get(i).getUsername().equals(raceUpdate.getUsername())) {
+                raceUpdates.set(i, raceUpdate);
+                return;
+            }
         }
+        raceUpdates.add(raceUpdate);
     }
 
     /**
@@ -108,10 +107,7 @@ public class Client implements Runnable{
         try {
             while (true) {
                 Object receivedObject = objectInputStream.readObject();
-                if(receivedObject instanceof User) {
-                    processMessage((User) receivedObject);
-                }
-                else if(receivedObject instanceof String) {
+                if(receivedObject instanceof String) {
                     processMessage((String) receivedObject);
                 }
                 else if(receivedObject instanceof Message) {
@@ -139,15 +135,6 @@ public class Client implements Runnable{
         Platform.runLater(() -> {
             messageBox.getChildren().add(label);
         });
-    }
-
-    /**
-     * @deprecated
-     * Process a User Object from the network
-     * @param currentUser
-     */
-    private void processMessage(User currentUser) {
-        user = user;
     }
 
     /**
