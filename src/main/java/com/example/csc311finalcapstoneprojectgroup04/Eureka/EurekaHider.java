79f4c073626fe15b5b6cb5b76901ad48ac59db10
MaximFarmingdale <nikim@farmingdale.edu>
2025-05-06 22:43:30 -0400

    Added a Eureka Hider bean that make sure that the instance is down on start-up

--- src/main/java/com/example/csc311finalcapstoneprojectgroup04/Eureka/EurekaHider.java
+++ src/main/java/com/example/csc311finalcapstoneprojectgroup04/Eureka/EurekaHider.java
@@ -3,13 +3,19 @@ import com.netflix.appinfo.ApplicationInfoManager;
 import com.netflix.appinfo.InstanceInfo;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 @Component
 public class EurekaHider {
-        @Autowired
-        private ApplicationInfoManager applicationInfoManager;
-        @PostConstruct
-        public void setInitialStatusToDown() {
-            applicationInfoManager.setInstanceStatus(InstanceInfo.InstanceStatus.DOWN);
-        }
+
+    @Autowired
+    private ApplicationInfoManager applicationInfoManager;
+
+    @PostConstruct
+    public void setInitialStatusToDown() {
+        System.out.println(">>> Forcing Eureka status to DOWN at startup");
+        applicationInfoManager.setInstanceStatus(InstanceInfo.InstanceStatus.DOWN);
     }
+}
