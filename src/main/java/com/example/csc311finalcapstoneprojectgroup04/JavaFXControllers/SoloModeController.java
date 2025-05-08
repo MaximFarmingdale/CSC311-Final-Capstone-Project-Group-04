fc60f431705f132e217cd32c609d477262c34213
hubao <kazmha@farmingdale.edu>
2025-05-07 20:24:38 -0400

    SoloMode.fxml v0.3 - added some more visuals for the middle racing node
    SoloModeController.java v0.2 - updated to reflect added fxids

--- src/main/java/com/example/csc311finalcapstoneprojectgroup04/JavaFXControllers/SoloModeController.java
+++ src/main/java/com/example/csc311finalcapstoneprojectgroup04/JavaFXControllers/SoloModeController.java
@@ -1,4 +1,30 @@
 package com.example.csc311finalcapstoneprojectgroup04.JavaFXControllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+
 public class SoloModeController {
+
+    @FXML
+    private TextField enterWordField;
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    private ListView<?> wordList;
+
+    @FXML
+    private Text wordsEnteredCounter;
+
+    @FXML
+    private Circle youNode;
+
+    @FXML
+    private Text youNodetext;
+
 }
